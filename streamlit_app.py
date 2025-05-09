06029d45518cf506e6c841a4bf652ec645c6fb33
sn2025120 <sn2025120@shs.hs.kr>
2025-05-09 11:22:13 +0000

    0101

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,54 @@ st.subheader("하위 제목입니다")
 
 
 
-name=st.text_input("이름을 입력해주세요")
-st.write(name+"님 반갑습니다!")
+import streamlit as st
+
+st.title("사용자 인증")
+
+# 이름 입력
+name = st.text_input("이름을 입력해주세요")
+
+# 성별 선택 (선택되지 않게 초기화)
+gender = st.radio("성별을 선택하세요", ["남성", "여성", "기타"], index=None)
+
+# 조건 검사
+if name:
+    if name == "유영준":
+        if gender is None:
+            st.warning("성별을 선택해주세요!")
+        else:
+            st.success(f"{name}님 반갑습니다! (성별: {gender})")
+    else:
+        st.error("접근 권한이 없습니다")
+else:
+    st.info("이름을 입력해주세요.")
 
 # 여러 옵션 중 하나 선택
-gender = st.radio("성별을 선택하세요", ["남성", "여성", "기타"])
-st.write("선택한 성별:", gender)
 
-if name=="유영준":
-    st.success(name+"님 반갑습니다")
+import streamlit as st
+
+st.title("성별 선택 예제")
+
+gender = st.radio("성별을 선택하세요", ["남성", "여성", "기타"], index=None)
+
+if gender is None:
+    st.warning("성별을 선택해주세요!")
 else:
-    st.error("접근 권한이 없습니다")
+    st.success(f"선택한 성별: {gender}")
+
+
+
+
+    # 날짜 입력
+date = st.date_input("날짜를 선택하세요")
+st.write("선택한 날짜:", date)
+
+# 시간 입력
+time = st.time_input("시간을 선택하세요")
+st.write("선택한 시간:", time)
+
+
+# 체크 여부에 따라 분기
+agree = st.checkbox("위 조건에 동의합니다")
+if agree:
+    st.write("감사합니다! 계속 진행합니다.")
